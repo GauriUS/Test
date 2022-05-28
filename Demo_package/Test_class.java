0e64d4cf402d6920166ffe1ebdfa51d6e9c2e3ed
GauriUS <106225208+GauriUS@users.noreply.github.com>
2022-05-28 13:22:16 +0530

    Add files via upload

---
+++ Demo_package/Test_class.java
@@ -0,0 +1,30 @@
+package Demo_package;
+
+public class Test_class {
+	
+	static byte a;
+	static short b;
+	static int c;
+	static long d;
+	static float f;
+	static double g;
+	static char m;
+	static boolean r;
+	static String v;
+	
+	public static void main(String[] args) 
+	{
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(d);
+		System.out.println(f);
+		System.out.println(g);
+		System.out.println(m);
+		System.out.println(r);
+		System.out.println(v);
+		
+		
+	}
+
+}
